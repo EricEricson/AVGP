945696e28ac40df39b1a72c7fbb1abec310360d4
EricEricson <mail.eric@gmx.de>
2019-11-01 12:31:53 +0100

    Updates

--- DirectSound/DirectSound/DirectSound/resource.h
+++ DirectSound/DirectSound/DirectSound/resource.h
@@ -4,15 +4,21 @@
 //
 #define IDD_DIRECTSOUND_DIALOG          102
 #define IDR_MAINFRAME                   128
-#define IDC_BUTTON1                     1000
+#define IDC_BUTTON_pcm                  1000
+#define IDC_BUTTON_264Hz                1001
+#define IDC_BUTTON_CDur_Tonleiter       1002
+#define IDC_SLIDER_Lautstaerke          1003
+#define IDC_SLIDER_Balance              1004
+#define IDC_BUTTON_CDur_Dreiklang       1005
+#define IDC_BUTTON_Exit                 1006
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
